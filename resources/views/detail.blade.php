df733e13b25e5adf34169e2a870392fe8e6829c1
PavLikh <lpa2@mail.ru>
2021-08-31 19:57:28 +0300

    edit detail page

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -2,16 +2,6 @@
 
 @section('title-block')Autoliga-lar @endsection
 
-@section('tagline')
-  @parent
-    <section class="tagline">
-      <div id="top">
-            <h1>Режим просмотра</h1>
-            <!-- <h3>это образ жизни!</h3> -->
-      </div>
-    </section>
-@endsection
-
 @section('nav-chain')
   	@parent
   	<span>Каталог </span><span class="nav_arrow inline-block">></span><span> {{ $data->mark }} {{ $data->model }}</span>
