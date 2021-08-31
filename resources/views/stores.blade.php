a52dee015945395790ca3b07799b8eed55005ffb
PavLikh <lpa2@mail.ru>
2021-08-31 19:59:29 +0300

    edit stores page

--- resources/views/stores.blade.php
+++ resources/views/stores.blade.php
@@ -2,15 +2,6 @@
 
 @section('title-block')Autoliga-lar @endsection
 
-@section('tagline')
-  @parent
-    <section class="tagline">
-      <div id="top">
-            <h1>Режим просмотра</h1>
-      </div>
-    </section>
-@endsection
-
 @section('content')
 <script>
     var data = @json($data);
